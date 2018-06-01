e8709d8047b65b45cbc1b8e3c712867b194e505d
chenguocaiyx <1396890578@qq.com>
2018-06-01 10:42:35 +0800

    给简历增加js

---
+++ resume/script/resume.js
@@ -0,0 +1,10 @@
+var chat = document.getElementById("chat");
+
+chat.onclick = function () {
+    var char_text = document.getElementById("chat_text");
+    char_text.innerHTML = "Hello GcChen";
+}
+var skill = document.getElementById("skill");
+    skill.onclick = function () {
+        console.log("双截棍、beatbox")
+    }
